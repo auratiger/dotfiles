8553b1a86d64d4821896425346f8046cdf988374
Georgi Georgiev <auratiger00@gmail.com>
2022-09-15 22:11:17 +0300

    created setup scripts for copying dotfiles

--- git-setup/setup.md
+++
@@ -1,6 +0,0 @@
-# Setup
-
-Copy the `.gitconfig` file in the root directory
-
-Copy the `gitcommands` directory in the `.config/` directory so `.gitconfig`
-will be able to find it.
---
+++ setup_docker_env.zsh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+DIR="$(cd "$(dirname "$0")" && pwd)"
+
+mkdir -p ~/.config 
+
+cp -r "${DIR}/.config" ~/
+
+zsh "${DIR}/.config/zsh/setup.zsh"
+zsh "${DIR}/git-setup/setup.zsh"
